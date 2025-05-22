07b96a7d4080eab1c31df9aab9acc3dda1829b8a
fancywhale23 <justintaoau@gmail.com>
2025-05-22 21:14:30 +1000

    added some command line args

---
+++ src/globals.hpp
@@ -0,0 +1,4 @@
+extern int quiet_flag;
+extern int num_threads;
+extern int hash_type;
+extern int atk_mode;
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <cstdlib>
+#include "globals.hpp"
+#include <cstdio>
 #include <cstring>
 #include <getopt.h>
 #include <memory>
 #include <openssl/evp.h>
+#include <string>
 
 struct EVP_free {
     void operator()(void *ptr) { EVP_MD_CTX_free((EVP_MD_CTX *)ptr); }
@@ -40,7 +42,72 @@ bool compute_hash(const char *input, char hashed[EVP_MAX_MD_SIZE * 2 + 1],
     return true;
 }
 
-int main(int argc, char *argv[]) {
-    return 0; 
-}
+int quiet_flag = 0;
+int num_threads = 1;
+int hash_type = -1;
+int atk_mode = -1;
 
+char mask_regex[] = "[^\\]?(\?.)";
+
+int main(int argc, char *argv[]) {
+    int opt_char;
+    while (1) {
+        static struct option long_opts[] = {
+            {"help", no_argument, 0, 'h'},
+            {"quiet", no_argument, 0, 'q'},
+            {"hash-type", required_argument, 0, 'm'},
+            {"attack-mode", required_argument, 0, 'a'},
+            {"threads", required_argument, 0, 't'},
+            {"custom-charset1", required_argument, 0, '1'},
+            {"custom-charset2", required_argument, 0, '2'},
+            {"custom-charset3", required_argument, 0, '3'},
+            {"custom-charset4", required_argument, 0, '4'},
+            {0, 0, 0, 0}
+
+        };
+
+        int opt_index = 0;
+        opt_char =
+            getopt_long(argc, argv, "hqm:a:t:1:2:3:4:", long_opts, &opt_index);
+
+        if (opt_char == -1)
+            break;
+
+        switch (opt_char) {
+        case 0:
+            break;
+        case 'h':
+            fprintf(stdout, "Usage: shikacracker -m <hash_type> -a <attack_mode> "
+                            "[options]... hashfile dictionary|mask\n");
+            break;
+        case 'q':
+            quiet_flag = 0;
+            break;
+        case 'm':
+            try {
+                hash_type = std::stoi(optarg);
+            } catch (std::exception err) {
+                fprintf(stderr, "Argument to -m must be an integer\n");
+            }
+            break;
+        case 'a':
+            try {
+                atk_mode = std::stoi(optarg);
+            } catch (std::exception err) {
+                fprintf(stderr, "Argument to -a must be an integer\n");
+            }
+            break;
+        case 't':
+            try {
+                num_threads = std::stoi(optarg);
+            } catch (std::exception err) {
+                fprintf(stderr, "Argument to -t must be an integer\n");
+            }
+            break;
+        case '?':
+            break;
+        default:
+            abort();
+        }
+    }
+}

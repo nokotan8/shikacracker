e64ef577fad98682cf689c99b65d90225605716c
fancywhale23 <justintaoau@gmail.com>
2025-05-30 22:34:30 +1000

    added multithreaded candidate generation for mask attack

--- src/mask_attack.cpp
+++ src/mask_attack.cpp
@@ -4,7 +4,9 @@
 #include "entry_buffer.hpp"
 #include "globals.hpp"
 #include "hash.hpp"
-#include "helpers.hpp"
+#include <algorithm>
+#include <functional>
+#include <iostream>
 #include <openssl/evp.h>
 #include <stdexcept>
 #include <string>
@@ -59,22 +61,82 @@ const std::vector<std::string> parse_mask() {
     return res;
 }
 
-void generate_pwd_candidates(std::string &curr_str,
-                             std::vector<std::string> &mask_format,
-                             entry_buffer<std::string> &buffer, int i,
-                             concurrent_set<std::string> &input_hashes) {
-    const int n = mask_format.size();
+void generate_pwd_candidates_cont(std::string &curr_str,
+                                  const std::vector<std::string> &mask_format,
+                                  entry_buffer<std::string> &buffer, size_t i,
+                                  concurrent_set<std::string> &input_hashes) {
+    const size_t n = mask_format.size();
     if (i == n) {
         if (input_hashes.empty())
             buffer.finished_add();
-        buffer.add_item(curr_str);
+        else
+            buffer.add_item(curr_str);
+        // fprintf(stdout, "%s\n", curr_str.c_str());
         return;
     }
 
     for (char c : mask_format[i]) {
         curr_str[i] = c;
-        generate_pwd_candidates(curr_str, mask_format, buffer, i + 1,
-                                input_hashes);
+        generate_pwd_candidates_cont(curr_str, mask_format, buffer, i + 1,
+                                     input_hashes);
+    }
+
+    return;
+}
+
+void generate_pwd_candidates_thread(std::string curr_str,
+                                    const std::vector<std::string> &mask_format,
+                                    entry_buffer<std::string> &buffer, size_t i,
+                                    size_t from_idx, size_t until_idx,
+                                    concurrent_set<std::string> &input_hashes) {
+    if (i >= mask_format.size())
+        return;
+
+    for (size_t j = from_idx; j <= until_idx; j++) {
+        curr_str[i] = mask_format[i][j];
+        generate_pwd_candidates_cont(curr_str, mask_format, buffer, i + 1,
+                                     input_hashes);
+    }
+}
+
+void generate_pwd_candidates(std::string curr_str,
+                             const std::vector<std::string> &mask_format,
+                             entry_buffer<std::string> &buffer,
+                             concurrent_set<std::string> &input_hashes) {
+    const size_t n = mask_format.size();
+    size_t i = 0;
+    for (; i < n && mask_format[i].size() < 2; i++) {
+        curr_str[i] = mask_format[i][0];
+    }
+
+    if (i == n) {
+        if (input_hashes.empty())
+            buffer.finished_add();
+        else
+            buffer.add_item(curr_str);
+        return;
+    }
+
+    size_t generate_num_threads =
+        std::min((size_t)num_threads, mask_format[i].size());
+    size_t thread_charspace = mask_format[i].size() / generate_num_threads;
+    std::vector<std::thread> generate_threads(generate_num_threads);
+
+    for (size_t j = 0; j < generate_num_threads; j++) {
+        size_t from_idx = j * thread_charspace;
+        size_t until_idx;
+        if (j == generate_num_threads - 1)
+            until_idx = mask_format[i].size() - 1;
+        else
+            until_idx = from_idx + thread_charspace - 1;
+
+        generate_threads[j] = std::thread(
+            generate_pwd_candidates_thread, curr_str, std::ref(mask_format),
+            std::ref(buffer), i, from_idx, until_idx, std::ref(input_hashes));
+    }
+
+    for (auto &thread : generate_threads) {
+        thread.join();
     }
 
     return;
@@ -88,7 +150,7 @@ void mask_attack(concurrent_set<std::string> &input_hashes) {
     std::vector<std::string> mask_format = parse_mask();
 
     std::vector<std::thread> cracker_threads(num_threads);
-    entry_buffer<std::string> buffer(num_threads * 2);
+    entry_buffer<std::string> buffer(num_threads * 4);
     const EVP_MD *hash_type = hash_mode_to_EVP_MD();
 
     for (int i = 0; i < num_threads; i++) {
@@ -97,11 +159,12 @@ void mask_attack(concurrent_set<std::string> &input_hashes) {
     }
 
     std::string curr_str(mask_format.size(), '\0');
-    generate_pwd_candidates(curr_str, mask_format, buffer, 0, input_hashes);
+    generate_pwd_candidates(curr_str, mask_format, buffer, input_hashes);
     buffer.finished_add();
+    // std::cout << "hi\n";
 
-    for (int i = 0; i < num_threads; i++) {
-        cracker_threads[i].join();
+    for (auto &thread : cracker_threads) {
+        thread.join();
     }
 
     if (input_hashes.size() != 0) {

4c5b74c2e217e3bed14069738a6d7fbc5b58bf28
fancywhale23 <justintaoau@gmail.com>
2025-06-06 22:02:03 +1000

    added c-style mask parsing for use in opencl

--- src/mask_attack.cpp
+++ src/mask_attack.cpp
@@ -3,60 +3,125 @@
 #include "concurrent_set.hpp"
 #include "entry_buffer.hpp"
 #include "globals.hpp"
-#include "hash.hpp"
-#include <functional>
-#include <openssl/evp.h>
+// #include "hash.hpp"
+
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <new>
 #include <stdexcept>
 #include <string>
-#include <thread>
 #include <vector>
 
-/**
- * Converts user-inputted mask string into a vector
- * of strings, where each string represents the possible
- * character(s) at each position.
- */
-const std::vector<std::string> parse_mask() {
-    std::vector<std::string> res;
+size_t parse_mask(const std::string &mask, char **charset,
+                  size_t **charset_offsets, size_t **charset_lengths,
+                  size_t *pwd_length) {
+    if (mask.empty()) {
+        throw std::invalid_argument("mask must be at least 1 character long");
+    } else if (pwd_length == NULL) {
+        throw std::invalid_argument("pwd_length cannot be NULL");
+    }
+
+    if (*charset == nullptr) {
+        free(*charset);
+        *charset = nullptr;
+    }
+    if (*charset_offsets == nullptr) {
+        free(*charset_offsets);
+        *charset_offsets = nullptr;
+    }
+    if (*charset_lengths == nullptr) {
+        free(*charset_lengths);
+        *charset_lengths = nullptr;
+    }
+
+    // First get length of password and mask
+    *pwd_length = 0;
+    size_t charset_len = 0;
     for (size_t i = 0; i < mask.size(); i++) {
-        if (mask[i] == '\\') {
+        if (mask[i] == '?') {
             if (i < mask.size() - 1) {
                 if (mask[i + 1] == '?') {
-                    res.push_back("?");
-                    i++;
-                } else if (mask[i + 1] == '\\') {
-                    res.push_back("\\");
+                    (*pwd_length)++;
+                    charset_len++;
                     i++;
                 } else {
-                    throw std::invalid_argument(
-                        "'" + mask.substr(i, 2) +
-                        "' is not a valid escape sequence");
-                }
-            } else {
-                throw std::invalid_argument(
-                    "'\\' at end of mask does not escape anything");
-            }
-        } else if (mask[i] == '?') {
-            if (i < mask.size() - 1) {
-                const std::string &charset = get_charset(mask[i + 1]);
-                if (charset.size() > 0) {
-                    res.push_back(charset);
-                    i++;
-                } else {
-                    throw std::invalid_argument(
-                        "'" + mask.substr(i, 2) +
-                        "' does not reference a valid charset");
+                    const std::string &curr_charset = get_charset(mask[i + 1]);
+                    if (curr_charset.empty() == false) {
+                        (*pwd_length)++;
+                        charset_len += curr_charset.size();
+                        i++;
+                    } else {
+                        throw std::invalid_argument(
+                            "'" + mask.substr(i, 2) +
+                            "' does not reference a valid charset");
+                    }
                 }
             } else {
                 throw std::invalid_argument(
                     "'?' at end of mask does not reference any charset");
             }
         } else {
-            res.push_back(std::string(1, mask[i]));
+            (*pwd_length)++;
+            charset_len++;
         }
     }
 
-    return res;
+    // Every element *should* be assigned a value, so malloc over calloc
+    *charset = (char *)malloc(sizeof(char) * charset_len);
+    if (*charset == nullptr) {
+        throw std::bad_alloc();
+    }
+    *charset_offsets = (size_t *)malloc(sizeof(size_t) * (*pwd_length));
+    if (*charset_offsets == nullptr) {
+        free(*charset);
+        *charset = nullptr;
+        throw std::bad_alloc();
+    }
+    *charset_lengths = (size_t *)malloc(sizeof(size_t) * (*pwd_length));
+    if (*charset_lengths == nullptr) {
+        free(*charset);
+        *charset = nullptr;
+        free(*charset_offsets);
+        *charset_offsets = nullptr;
+        throw std::bad_alloc();
+    }
+
+    // Then populate the arrays. Error checking already done.
+    size_t cset_idx = 0;
+    size_t cset_len_offset_idx = 0;
+    size_t cset_offset = 0;
+    for (size_t i = 0; i < mask.size();
+         i++, cset_idx++, cset_len_offset_idx++) {
+        if (mask[i] == '?') {
+            if (mask[i + 1] == '?') {
+                (*charset)[cset_idx] = '?';
+                (*charset_lengths)[cset_len_offset_idx] = 1;
+                (*charset_offsets)[cset_len_offset_idx] = cset_offset;
+                cset_offset++;
+                i++;
+            } else {
+                const std::string &curr_charset = get_charset(mask[i + 1]);
+                for (char c : curr_charset) {
+                    (*charset)[cset_idx] = c;
+                    cset_idx++;
+                }
+                cset_idx--;
+
+                (*charset_lengths)[cset_len_offset_idx] = curr_charset.size();
+                (*charset_offsets)[cset_len_offset_idx] = cset_offset;
+                cset_offset += curr_charset.size();
+                i++;
+            }
+        } else {
+            (*charset)[cset_idx] = mask[i];
+            (*charset_lengths)[cset_len_offset_idx] = 1;
+            (*charset_offsets)[cset_len_offset_idx] = cset_offset;
+            cset_offset++;
+        }
+    }
+
+    return charset_len;
 }
 
 /**
@@ -90,32 +155,59 @@ void generate_pwd_candidates(std::string &curr_str,
  * Performs a mask attack, similar to:
  * https://hashcat.net/wiki/doku.php?id=mask_attack
  */
-void mask_attack(concurrent_set<std::string> &input_hashes) {
-    std::vector<std::string> mask_format = parse_mask();
+void mask_attack(const std::string &mask,
+                 concurrent_set<std::string> &input_hashes) {
+    char *charset = nullptr;
+    size_t *charset_offsets = nullptr;
+    size_t *charset_lengths = nullptr;
+    size_t pwd_length;
 
-    std::vector<std::thread> cracker_threads(num_threads);
-    entry_buffer<std::string> buffer(num_threads * 4);
-    const EVP_MD *hash_type = hash_mode_to_EVP_MD();
+    size_t charset_len = parse_mask(mask, &charset, &charset_offsets,
+                                    &charset_lengths, &pwd_length);
 
-    for (int i = 0; i < num_threads; i++) {
-        cracker_threads[i] = std::thread(hash_thread, std::ref(buffer),
-                                         std::ref(input_hashes), hash_type);
-    }
+    /* For testing */
+    // for (size_t i = 0; i < pwd_length; i++) {
+    //     fprintf(stdout, "%zu ", charset_offsets[i]);
+    // }
+    // fprintf(stdout, "\n");
+    //
+    // for (size_t i = 0, curr_pos = 1; i < charset_len; i++) {
+    //     if (curr_pos < pwd_length && i == charset_offsets[curr_pos]) {
+    //         fprintf(stdout, "\n");
+    //         curr_pos++;
+    //     }
+    //
+    //     fprintf(stdout, "%c ", charset[i]);
+    // }
+    // fprintf(stdout, "\n");
 
-    std::string curr_str(mask_format.size(), '\0');
-    generate_pwd_candidates(curr_str, mask_format, buffer, 0, input_hashes);
-    buffer.finished_add();
+    // std::vector<std::thread> cracker_threads(num_threads);
+    // entry_buffer<std::string> buffer(num_threads * 4);
+    // const EVP_MD *hash_type = hash_mode_to_EVP_MD();
+    //
+    // for (int i = 0; i < num_threads; i++) {
+    //     cracker_threads[i] = std::thread(hash_thread, std::ref(buffer),
+    //                                      std::ref(input_hashes), hash_type);
+    // }
+    //
+    // std::string curr_str(mask_format.size(), '\0');
+    // generate_pwd_candidates(curr_str, mask_format, buffer, 0, input_hashes);
+    // buffer.finished_add();
+    //
+    // for (auto &thread : cracker_threads) {
+    //     thread.join();
+    // }
 
-    for (auto &thread : cracker_threads) {
-        thread.join();
-    }
+    // if (input_hashes.size() != 0) {
+    //     fprintf(stdout, "Hashes not cracked:\n");
+    //     for (const auto &hash : input_hashes.set) {
+    //         fprintf(stdout, "%s\n", hash.c_str());
+    //     }
+    // }
 
-    if (input_hashes.size() != 0) {
-        fprintf(stdout, "Hashes not cracked:\n");
-        for (const auto &hash : input_hashes.set) {
-            fprintf(stdout, "%s\n", hash.c_str());
-        }
-    }
+    free(charset);
+    free(charset_offsets);
+    free(charset_lengths);
 
     return;
 }
--- src/mask_attack.hpp
+++ src/mask_attack.hpp
@@ -1,5 +1,10 @@
-#pragma once
+#ifndef INC_MASK_ATTACK_H
+#define INC_MASK_ATTACK_H
+
 #include "concurrent_set.hpp"
 #include <string>
 
-void mask_attack(concurrent_set<std::string> &input_hashes);
+void mask_attack(const std::string &mask,
+                 concurrent_set<std::string> &input_hashes);
+
+#endif // INC_MASK_ATTACK_H
